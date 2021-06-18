65aa3f10a47f104c7c3d39759da7fd10554a028a
Todd Pickelll <todd@tap-software.com>
2021-06-17 20:53:06 -0500

    adds readme and connects poncho app

--- grow_tent_firmware/mix.exs
+++ grow_tent_firmware/mix.exs
@@ -35,6 +35,7 @@ defmodule GrowTent.MixProject do
       {:shoehorn, "~> 0.7.0"},
       {:ring_logger, "~> 0.8.1"},
       {:toolshed, "~> 0.2.13"},
+      {:grow_tent, path: "../grow_tent", targets: @all_targets, env: Mix.env()},
 
       # Dependencies for all targets except :host
       {:nerves_runtime, "~> 0.11.3", targets: @all_targets},
